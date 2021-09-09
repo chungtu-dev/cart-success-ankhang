b0cc5b6e1f9e59c013b991aae4e6bb1ca5ee18da
DESKTOP-I5I7HDF\Admin <nguyentutsq@gmail.com>
2021-09-09 18:57:24 +0700

    thêm BoxOrder.vue và ListProductsInBoxOrder.vue đang xử lý phần phần đặt sản phẩm

---
+++ src/common/components/box-order/BoxOrder.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="box-order">
+      <ListProductsInBoxOrder :listProduct="deliveryInfo.listProduct"/>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import {receiptMethodType} from '@/common/types';
+import ListProductsInBoxOrder from './ListProductsInBoxOrder.vue';
+import {CUSTOMER_INFO_MODULE_NAME,
+    SEX_STRING} from '@/store/module-types/page-cart/customerInfo'
+export default {
+    components:{
+        ListProductsInBoxOrder
+    },
+    props: {
+        deliveryInfo: {
+            type: Object,
+            required: true,
+        },
+        receiptMethod: {
+            type: Number,
+            required: false,
+            default: receiptMethodType.undefined
+        },
+        cartType: {
+            required: false,
+            default: '',
+        },
+    },
+    computed:{
+        ...mapGetters(CUSTOMER_INFO_MODULE_NAME,[SEX_STRING]),
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/common/components/box-order/ListProductsInBoxOrder.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul>
+      <li v-for="(product, index) in listProduct" :key="index">
+        <a :href="product.url" target="_blank" class="img-order">
+            <img :data-src="product.thumbnail" :src="product.thumbnail" loading="lazy" class="lazyload">
+        </a>
+        <div class="text-order">
+            <a class="text-order__product-name" :href="product.url" target="_blank">{{product.name}}</a>
+            <div class="amount-color">
+                <small v-if="$isEmptyOrNil(product.colorName)===false">Màu: <small>{{product.colorName}}</small></small>
+                <small>Số lượng: {{product.quantity}}</small>
+            </div>
+        </div>
+      </li>
+  </ul>
+</template>
+
+<script>
+export default {
+    props:{
+        listProduct:{
+            required:false,
+            default:()=>[],
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
