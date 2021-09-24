002d6e06ff465408edc3f1ddfb7dd974770c0cab
DESKTOP-I5I7HDF\Admin <nguyentutsq@gmail.com>
2021-09-24 11:31:47 +0700

    update get {customer_sex} {customer_name} trong popup đếm ngược

--- src/common/components/popup/PopupCancelOrder.vue
+++ src/common/components/popup/PopupCancelOrder.vue
@@ -1,62 +1,92 @@
 <template>
   <div>
     <div class="popup-cancelod">
-    <div class="bg-popup"></div>
-    <div class="cancel-order">
-      <i class="closepopup"></i>
-      <p>
-        NhathuocAnKhang.com mong nhận được góp ý của anh để cải thiện chất lượng
-        dịch vụ tốt hơn!
-      </p>
-      <span v-for="item in listReason" :key="item.text">
-          <i class="ankhang-stick" v-bind:class="{active: item.active}" @click="item.active =! item.active"></i>          
-          {{item.text}}  
-      </span>
-      <textarea
-        v-model="note"
-        placeholder="Nhập nội dung góp ý"
-      ></textarea>
-      <div class="btn-popup">
-        <a href="javascript:;" class="none-del"> KHÔNG HUỶ </a>
-        <a href="javascript:;" class="accept-del" 
-        @click="removeOrderResult()" >
-          XÁC NHẬN <br />HUỶ ĐƠN HÀNG
-        </a>
-      </div>      
-    </div>    
-  </div>
-  <popup-waiting-close v-if="isCancelSuccess"/>
+      <div class="bg-popup"></div>
+      <div class="cancel-order">
+        <i class="closepopup"></i>
+        <p>
+          NhathuocAnKhang.com mong nhận được góp ý của anh để cải thiện chất
+          lượng dịch vụ tốt hơn!
+        </p>
+        <span v-for="item in listReason" :key="item.text">
+          <i
+            class="ankhang-stick"
+            v-bind:class="{ active: item.active }"
+            @click="item.active = !item.active"
+          ></i>
+          {{ item.text }}
+        </span>
+        <!-- <span v-for="item in listReason" :key="item.text">
+      <div >
+        <li>
+          <input type="checkbox" v-model="item.active">
+          {{item.text}}
+        </li>
+      </div>
+    </span> -->
+        <textarea v-model="note" placeholder="Nhập nội dung góp ý"></textarea>
+        <div class="btn-popup">
+          <a href="javascript:;" class="none-del"> KHÔNG HUỶ </a>
+          <a
+            href="javascript:;"
+            class="accept-del"
+            @click="removeOrderResult()"
+          >
+            XÁC NHẬN <br />HUỶ ĐƠN HÀNG
+          </a>
+        </div>
+      </div>
+    </div>
+    <popup-waiting-close v-if="isCancelSuccess" />
   </div>
 </template>
 <script>
 import * as c_types from "@/store/module-types/page-cart/customerInfo";
 import { mapActions } from "vuex";
-import PopupWaitingClose from '@/common/components/popup/PopupWaitingClose.vue';
+import PopupWaitingClose from "@/common/components/popup/PopupWaitingClose.vue";
 export default {
-  components: {PopupWaitingClose},
+  components: { PopupWaitingClose },
   data() {
     return {
-      isCancelSuccess:false,
-      isShowPopupCancel:false,
-        note: null,
-        listReason: [
-          { text: "Tôi không còn nhu cầu", active: false },
-          { text: "Tôi muốn mua đơn hàng khác", active: false },
-          { text: "Tôi tìm chỗ khác giá tốt hơn", active: false },
-        ], 
+      isCancelSuccess: false,
+      isShowPopupCancel: false,
+      note: null,
+      listReason: [
+        { text: "Tôi không còn nhu cầu", active: false },
+        { text: "Tôi muốn mua đơn hàng khác", active: false },
+        { text: "Tôi tìm chỗ khác giá tốt hơn", active: false },
+      ],
     };
   },
   methods: {
     ...mapActions([c_types.REMOVE_ORDER_RESULT]),
-    removeOrderResult() {        
-      const textReason = this.note + " - " + this.listReason.filter(el => el.active === true)
-      .reduce((prev, {text}) => prev + (prev ? " - ": "") + text, "");   
-      console.log("Reason",textReason);    
+    removeOrderResult() {
+      const textReason =
+        this.note +
+        " - " +
+        this.listReason
+          .filter((el) => el.active === true)
+          .reduce((prev, { text }) => prev + (prev ? " - " : "") + text, "");
+      console.log("Lý do hủy đơn", textReason);
       this.REMOVE_ORDER_RESULT({
-        note: this.note ? textReason + " - " + this.note : textReason,        
-      })
+        note: this.note ? textReason + " - " + this.note : textReason,
+      });
       this.isCancelSuccess = true;
     },
+
+    // handleSubmit(){
+    //   let reasonIsTrue = this.listReason.filter(e => e.active === true);
+    //   let temp="";
+    //   reasonIsTrue.forEach(e =>{
+    //     if(temp != ''){
+    //       temp = `${temp} - ${e.text}`
+    //     }
+    //     else{
+    //       temp = e.text
+    //     }
+    //     console.log(temp, this.note)
+    //   })
+    // },
   },
 };
 </script>
--- src/common/components/popup/PopupWaitingClose.vue
+++ src/common/components/popup/PopupWaitingClose.vue
@@ -5,18 +5,18 @@
 			<i class="closepopup"></i>
 			<strong>HUỶ ĐƠN HÀNG THÀNH CÔNG</strong>
 			<p>
-				Cảm ơn anh Nghiêng đã đóng góp để giúp nhà thuốc AnKhang cải tiến chất lượng dịch vụ tốt hơn! 
+				Cảm ơn {{CUSTOMER_INFO.customerSex}} {{CUSTOMER_INFO.customerName}} đã đóng góp để giúp nhà thuốc AnKhang cải tiến chất lượng dịch vụ tốt hơn! 
 			</p>
-
 			<!-- <button @click="closePopupCancel">ĐÓNG</button> -->
             <a href="javascript:;" @click="closePopupCancel">ĐÓNG</a>
-
 			<small>Tự động đóng trong {{countTemp}} giây</small>
 		</div>
 	</div>
 </template>
 
 <script>
+import {mapGetters} from 'vuex';
+import * as c_types from "@/store/module-types/page-cart/customerInfo";
 export default {
     data(){
         return{
@@ -24,13 +24,15 @@ export default {
             countTemp: 5,
         }
     },
+    computed:{ 
+        ...mapGetters([c_types.CUSTOMER_INFO]),
+    },
     methods: {
         closePopupCancel(){
             this.countDown();
             this.isClosePopupCancel = false;
             window.location.href = "https://staging.nhathuocankhang.com/html/v2.1/home.html";
-        },
-
+        },    
         countDown(){
             if(this.countTemp > 0){
                 setTimeout(()=>{
